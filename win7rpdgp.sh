d9f8ea3619deef263f811ad1806bfd5c7609d08b
uncleluogithub <90886354+uncleluogithub@users.noreply.github.com>
2021-10-04 15:58:46 +0800

    Update win7rpdgp.sh

--- win7rpdgp.sh
+++ win7rpdgp.sh
@@ -1,6 +1,6 @@
 apt-get update
 echo "下载Windows7镜像文件爽歪歪！～"
-wget -O w7x64.img https://bit.ly/uncleluo-win7x64img
+wget -O w10x64.img https://app.vagrantup.com/senglin/boxes/win-10-enterprise-vs2015community/versions/1.0.0/providers/virtualbox.box
 echo "下载ngrok远程连接组件酷毙了！～"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
@@ -11,7 +11,7 @@ echo 从Uncle LUO老罗叔叔这里下载文件是不是感觉很爽啊？：）
 apt-get install qemu-system-x86 -y
 echo "稍安勿躁！～"
 echo "开始等待Windows7发功了！"
-qemu-system-x86_64 -hda w7x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda w10x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
