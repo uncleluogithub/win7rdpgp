6bb5e296d7df2136b7321b2989eda570a617c8ba
uncleluogithub <90886354+uncleluogithub@users.noreply.github.com>
2021-10-11 19:13:35 +0800

    Update win7rpdgp.sh

--- win7rpdgp.sh
+++ win7rpdgp.sh
@@ -1,7 +1,7 @@
 apt-get update
-echo "同学们，朋友们，罗叔变戏法开始了！~现在在下载Windows文件中，请等候约3分钟左右时间..."
+echo "同学们，朋友们，罗叔变戏法开始了！~现在在下载Windows文件中，请等候约2分钟左右时间..."
 wget -O win7.img https://link.jscdn.cn/sharepoint/aHR0cHM6Ly9jYW1wdXNtY2NkLW15LnNoYXJlcG9pbnQuY29tLzp1Oi9nL3BlcnNvbmFsL2xpbmRhX3JpY2hfY2FtcHVzX21jY2RfZWR1L0VSTW5BdjZUVDlsRnNPYzljSGJhaVZJQjN0dHdQM0RzZDZ1OGFBTXpYa0c3N2c_ZT1WWmEyUng.img
-echo "亲们，而今在下载ngrok远程连接组件中..."
+echo "朋友们！下载ngrok远程连接组件中..."
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "粘贴key在这里，最后记得按回车确认，先请各位按下Ctrl + V来粘贴在ngrok网站中所复制的Authtoken: " CRP 
@@ -16,7 +16,7 @@ clear
 echo 请复制这个远程桌面RDP登录地址:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "用户名: uncleluo"
+echo "用户名: Administrator"
 echo "密码: uncleluo"
 echo "===================================="
 echo "===================================="
