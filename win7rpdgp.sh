e5ec15d800d4b4967a04efb925ea44eac66fa028
uncleluogithub <90886354+uncleluogithub@users.noreply.github.com>
2023-06-22 12:05:13 +0800

    Update win7rpdgp.sh

--- win7rpdgp.sh
+++ win7rpdgp.sh
@@ -1,6 +1,6 @@
 apt-get update
 echo "同学们，朋友们，罗叔变戏法开始了！~现在在下载Windows文件中，请等候约1分钟左右时间..."
-wget -O win7.img https://link.jscdn.cn/sharepoint/aHR0cHM6Ly9jYW1wdXNtY2NkLW15LnNoYXJlcG9pbnQuY29tLzp1Oi9nL3BlcnNvbmFsL2xpbmRhX3JpY2hfY2FtcHVzX21jY2RfZWR1L0VSTW5BdjZUVDlsRnNPYzljSGJhaVZJQjN0dHdQM0RzZDZ1OGFBTXpYa0c3N2c_ZT1WWmEyUng.img
+wget -O win7.img https://link.jscdn.cn/sharepoint/aHR0cHM6Ly9qb2hubHVvZ2EtbXkuc2hhcmVwb2ludC5jb20vOnU6L2cvcGVyc29uYWwvb25lZHJpdmVfdW5jbGVsdW9fY29tL0VWYlRBSXhtdmU1UHQ1dnhxMlZKMms0QlZISWZ4bWFZdVdTVmhKTFNtMmlFalE_ZT1aY1ZGbDc.img
 echo "朋友们！下载ngrok远程连接组件中..."
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
@@ -20,7 +20,7 @@ echo "用户名: Administrator"
 echo "密码: uncleluo"
 echo "===================================="
 echo "===================================="
-echo "感谢您持续关注+-Uncle LUO老罗叔叔的数字化生活-+YouTube频道"
+echo "感谢您持续关注+-Uncle LUO老罗叔叔的数字生活-+YouTube频道"
 echo "你快要获得了Windows远程桌面连接了..."
 echo "再等待2分钟左右的时间等待系统完成最后一个配置吧~"
 echo "你可以关闭本标签栏，但不要忘记关注我的YouTube频道，有及时更新需求请打开小铃铛哦！~"
